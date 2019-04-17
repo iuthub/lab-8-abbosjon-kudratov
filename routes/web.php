157c5e5c6ca5dc235577cd5931dfd451d0eaf761
abbosjon-kudratov <abbosjon.kudratov@gmail.com>
2019-04-18 03:03:21 +0500

    still some ptoblems with routes

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog.index');
+});
+
+Route::get('/post/{id}', function ($id) {
+    return view('blog.post')->name('blog.post')->with($id);
+
 });
